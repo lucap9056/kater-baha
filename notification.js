0bd06f62763228e81673c5e6eb6f41371c1d82f9
123ldkop <jim12366219@gmail.com>
2022-11-11 23:38:21 +0800

    Create notification.js

---
+++ notification.js
@@ -0,0 +1,18 @@
+(() => {
+    var temp;
+    (function getNotification() {
+        const xhr = XMLHttpRequest();
+        xhr.open("GET",'/api/notifications?page%5Boffset%5D=0&include');
+        xhr.onload = () => {
+            const res = xhr.response;
+            const first = JSON.stringify(res.data[0]);
+            if (temp != null && temp != first) {
+                if (temp == first) return;
+                temp = first;
+                postMessage(res);
+            }
+            else temp = first;
+            setTimeout(getNotification,60000);
+        }
+    })();
+})();
